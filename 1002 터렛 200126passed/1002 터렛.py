3c9b86c78191d82ed6b04347dc77aae11a1ed438
김시영 <ssks1013@gmail.com>
2020-01-26 20:39:40 +0900

    터렛 passed

--- 1002 터렛 200126passed/1002 터렛.py
+++ 1002 터렛 200126passed/1002 터렛.py
@@ -9,10 +9,25 @@ if not INPUTMODE:
     input = f.readline
 ################################   
 
+import math
+
 T = int(input())
 
 for _ in range(T):
     x1, y1, r1, x2, y2, r2 = map(int,input().split())
     dist = ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+
+    R = max(r1,r2)
+    r = min(r1,r2)
+
+    if math.isclose(0,dist) and r == R:
+        print(-1)
+    elif math.isclose(dist,r+R) or math.isclose(dist,R-r):
+        print(1)
+    elif dist > r + R or dist < R - r:
+        print(0)
+    else:
+        print(2)
+                    
 
    
