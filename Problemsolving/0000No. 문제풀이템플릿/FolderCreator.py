826d95d9fb5848ea5140bb3a7e3800e2bb0e2dfd
김시영 <ssks1013@gmail.com>
2020-02-02 15:58:39 +0900

    template 수정

--- Problemsolving/0000No. 문제풀이템플릿/FolderCreator.py
+++ Problemsolving/0000No. 문제풀이템플릿/FolderCreator.py
@@ -1,10 +1,11 @@
 import os
+import sys
 
 namefile = open('문제번호와 이름.txt','r',encoding='UTF8')
 name = namefile.readline().rstrip()
 namefile.close()
 
-os.mkdir('/Users/nnamo/Desktop/Algorithms-and-Problem-Solving/Problemsolving/'+name+'/')
+os.mkdir('../'+name+'/')
 
 firstinput = '#' + name +'\n'
 firstinput += """
@@ -15,9 +16,9 @@ INPUTMODE = 0
 if not INPUTMODE:
     f = open("input.txt","r")
     input = f.readline
-################################   
+################################
 """
 
-pythonfile = open('/Users/nnamo/Desktop/Algorithms-and-Problem-Solving/Problemsolving/'+name+'/'+name+'.py','w',encoding='UTF8')
+pythonfile = open('../'+name+'/'+name+'.py','w',encoding='UTF8')
 pythonfile.write(firstinput)
 pythonfile.close()
